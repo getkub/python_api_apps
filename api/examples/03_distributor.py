1eb8e44f6a80e949529c9dd2b378f63ee14bced8
getkub <kinbsk@gmail.com>
2021-12-09 13:35:32 +0000

    Updated p03

--- api/examples/03_distributor.py
+++ api/examples/03_distributor.py
@@ -4,26 +4,25 @@ import sys
 sys.path.insert(0,'../..')
 from importlib import import_module
 
+from python_apps.p03_distributor import caller
+
 app = FastAPI()
 
+# Creates new instance.
+my_instance = caller.p03_distributor()
+
 # Defaults
-app_to_run="app_01"
-app_fn="app_01_fn"
+app_to_run="app_00"
+app_fn="app_00_fn"
 
 @app.get("/")
 def read_root():
-    from python_apps.p03_distributor import caller
-    # Creates new instance.
-    p03_distributor_instance = caller.p03_distributor()
-    return (p03_distributor_instance.run_distributor(app_to_run,app_fn))
+    return (my_instance.run_distributor(app_to_run,app_fn))
 
 @app.get("/apps/{item_id}")
 def read_item(item_id: str):
-    from python_apps.p03_distributor import caller
     app_to_run=item_id
     app_fn=item_id+"_fn"
-    # Creates new instance.
-    p03_distributor_instance = caller.p03_distributor()
-    return (p03_distributor_instance.run_distributor(app_to_run,app_fn))
+    return (my_instance.run_distributor(app_to_run,app_fn))
 
 # uvicorn 03_distributor:app --reload
