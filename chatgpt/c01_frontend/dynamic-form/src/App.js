74d75f64df3dcf80810b40962968e072ce4d8f4d
kin bsk <kinbsk@gmail.com>
2023-06-03 22:07:34 +0100

    Updated dynamic form

--- chatgpt/c01_frontend/dynamic-form/src/App.js
+++ chatgpt/c01_frontend/dynamic-form/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Form from './components/Form';
 import SuccessModal from './components/SuccessModal';
+import './dynamic-form.css'; // Import the CSS file
 
 const App = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -15,7 +16,7 @@ const App = () => {
 
   return (
     <Router>
-      <div>
+      <div className="app-container"> {/* Add the app-container class */}
         <h1>Dynamic Form</h1>
         <Switch>
           <Route exact path="/">
--- chatgpt/c01_frontend/dynamic-form/src/components/Form.js
+++ chatgpt/c01_frontend/dynamic-form/src/components/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../dynamic-form.css'; // Import your CSS file
 
 const Form = ({ onSubmit }) => {
   const [formData, setFormData] = useState({
@@ -9,6 +10,7 @@ const Form = ({ onSubmit }) => {
   });
 
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -20,31 +22,31 @@ const Form = ({ onSubmit }) => {
     axios
       .post('http://localhost:8000/api/submit-form', formData)
       .then((response) => {
-        // Handle success
         setIsSubmitted(true);
-        onSubmit(); // Call the onSubmit function from App.js
+        onSubmit();
       })
       .catch((error) => {
-        // Handle error
         console.error(error);
+        setIsError(true);
       });
   };
 
   return (
-    <div>
-      <h2>Submit Form</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
+    <div className="form-container">
+      <h2 className="form-header">Submit Form</h2>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="form-group">
           <label htmlFor="name">Name:</label>
           <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="email">Email:</label>
           <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" className="form-button">Submit</button>
       </form>
-      {isSubmitted && <p>Form submitted successfully!</p>}
+      {isSubmitted && <p className="success-message">Form submitted successfully!</p>}
+      {isError && <p className="error-message">An error occurred. Please try again.</p>}
     </div>
   );
 };
