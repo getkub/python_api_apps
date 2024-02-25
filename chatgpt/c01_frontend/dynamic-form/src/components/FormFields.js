e1f437cd6f4ab917500caa03a70dd6b6f304f3d9
getkub <getkub@get.kub>
2024-02-25 23:33:33 +0000

    better beautify

--- chatgpt/c01_frontend/dynamic-form/src/components/FormFields.js
+++ chatgpt/c01_frontend/dynamic-form/src/components/FormFields.js
@@ -10,41 +10,29 @@ const FormFields = ({ formData, setFormData }) => {
   };
 
   return (
-    <div className="row g-3">
-      <div className="col-md-6">
-        <label htmlFor="name" className="form-label">
-          Name:
-        </label>
+    <div className="form-fields">
+      <div className="form-field">
+        <label>Name:</label>
         <input
           type="text"
-          className="form-control"
-          id="name"
           name="name"
           value={formData.name || ''}
           onChange={handleChange}
         />
       </div>
-      <div className="col-md-6">
-        <label htmlFor="email" className="form-label">
-          Email:
-        </label>
+      <div className="form-field">
+        <label>Email:</label>
         <input
           type="text"
-          className="form-control"
-          id="email"
           name="email"
           value={formData.email || ''}
           onChange={handleChange}
         />
       </div>
-      <div className="col-md-6">
-        <label htmlFor="phone" className="form-label">
-          Phone:
-        </label>
+      <div className="form-field">
+        <label>Phone:</label>
         <input
           type="text"
-          className="form-control"
-          id="phone"
           name="phone"
           value={formData.phone || ''}
           onChange={handleChange}
